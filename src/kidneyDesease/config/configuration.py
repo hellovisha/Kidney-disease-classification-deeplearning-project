59314ce8cd506cc554d2e613eecd09e4049a13ec
hellovisha <vishalsinghrawat505@gmail.com>
2024-07-30 18:35:58 +0530

    "complete"

--- src/kidneyDesease/config/configuration.py
+++ src/kidneyDesease/config/configuration.py
@@ -1,7 +1,7 @@
-from kidneyDesease.constants import *
+from src.kidneyDesease.constants import *
 import os
-from kidneyDesease.utils.common import read_yaml, create_directories,save_json
-from kidneyDesease.entity.config_entity import (DataIngestionConfig,
+from src.kidneyDesease.utils.common import read_yaml, create_directories,save_json
+from src.kidneyDesease.entity.config_entity import (DataIngestionConfig,
                                                 PrepareBaseModelConfig,
                                                 TrainingConfig,
                                                 EvaluationConfig)
@@ -83,12 +83,12 @@ class ConfigurationManager:
         return training_config
     
 
-
+ 
     def get_evaluation_config(self) -> EvaluationConfig:
         eval_config = EvaluationConfig(
             path_of_model="artifacts/training/model.h5",
             training_data="artifacts/data_ingestion/kidney-ct-scan-image",
-            mlflow_uri="https://dagshub.com/entbappy/Kidney-Disease-Classification-MLflow-DVC.mlflow",
+            mlflow_uri="https://dagshub.com/hellovisha/Kidney-disease-classification.mlflow",
             all_params=self.params,
             params_image_size=self.params.IMAGE_SIZE,
             params_batch_size=self.params.BATCH_SIZE
